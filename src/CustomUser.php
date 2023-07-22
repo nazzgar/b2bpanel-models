68457a5718fd879a2b266ccbfcc6114a00711a2d
Mateusz Kozłowski <mateuszkozlowski0@gmail.com>
2023-07-22 14:02:13 +0200

    name change

--- src/CustomUser.php
+++ src/CustomUser.php
@@ -8,7 +8,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class CustomUser extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
