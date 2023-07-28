57eb9eaf1ae6572023e4c65f8fa9824b47b1e66b
Mateusz Kozłowski <mateuszkozlowski0@gmail.com>
2023-07-28 20:39:33 +0200

    bez prawa zwrotu product model

---
+++ src/BezPrawaZwrotuProduct.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace B2BPanel\SharedModels;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class BezPrawaZwrotuProduct extends Model
+{
+
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'symkar';
+
+    /**
+     * The data type of the auto-incrementing ID.
+     *
+     * @var string
+     */
+    protected $keyType = 'string';
+
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'symkar',
+        'kodkres',
+        'opis',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'isZwrotne' => 'boolean',
+    ];
+
+}
