3540132086e75221d1c3db421ef34d0f2a8f7db1
Mateusz Kozłowski <m.kozlowski@polanglo.pl>
2023-09-28 15:30:20 +0200

    bez_prawa_zwrotu_products in lins query

--- src/Services/LinsService.php
+++ src/Services/LinsService.php
@@ -11,7 +11,7 @@ class LinsService
     {
         $contractor = $user->contractors()->first();
 
-        return collect(DB::select("select n.numer, l.lp, n.logo, n.data, l.kodkres, l.symkar, l.opis, l.ilosc, l.cena_brutto_po_rabacie as brutto, l.cena_netto_po_rabacie as netto, l.PD_typoferty as typ_oferty, p.opis as wydawnictwo from lins l inner join nags n on n.nagid = l.nagid inner join publishers p on p.grupa=l.PD_Wydawnictwo where n.logo = :logo order by n.numer, l.lp", ["logo" => $contractor->logo]));
+        return collect(DB::select("select n.numer, l.lp, n.logo, n.data, l.kodkres, l.symkar, l.opis, l.ilosc, l.cena_brutto_po_rabacie as brutto, l.cena_netto_po_rabacie as netto, l.PD_typoferty as typ_oferty, p.opis as wydawnictwo from lins l inner join nags n on n.nagid = l.nagid inner join publishers p on p.grupa=l.PD_Wydawnictwo left join bez_prawa_zwrotu_products bp on bp.symkar = l.symkar where n.logo = :logo and (bp.isZwrotne is null or bp.isZwrotne = 1) order by n.numer, l.lp", ["logo" => $contractor->logo]));
     }
 
 
@@ -22,6 +22,6 @@ class LinsService
     {
         $contractor = $user->contractors()->first();
 
-        return collect(DB::select("select n.numer, l.lp, n.logo, n.data, l.kodkres, l.symkar, l.opis, l.ilosc, l.cena_brutto_po_rabacie as brutto, l.cena_netto_po_rabacie as netto, l.PD_typoferty as typ_oferty, p.opis as wydawnictwo from lins l inner join nags n on n.nagid = l.nagid inner join publishers p on p.grupa=l.PD_Wydawnictwo where n.logo = :logo and n.is_returnable = 1 order by n.numer, l.lp", ["logo" => $contractor->logo]));
+        return collect(DB::select("select n.numer, l.lp, n.logo, n.data, l.kodkres, l.symkar, l.opis, l.ilosc, l.cena_brutto_po_rabacie as brutto, l.cena_netto_po_rabacie as netto, l.PD_typoferty as typ_oferty, p.opis as wydawnictwo from lins l inner join nags n on n.nagid = l.nagid inner join publishers p on p.grupa=l.PD_Wydawnictwo left join bez_prawa_zwrotu_products bp on bp.symkar = l.symkar where n.logo = :logo and (bp.isZwrotne is null or bp.isZwrotne = 1) and n.is_returnable = 1 order by n.numer, l.lp", ["logo" => $contractor->logo]));
     }
 }
